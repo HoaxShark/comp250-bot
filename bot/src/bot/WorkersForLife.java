526989ee7bac5012375d8b63bfd7bd2150777ac3
hoaxshark <hoaxshark@live.com>
2019-05-06 11:22:29 +0100

    All comments now doxygen compatible some minor changes to AI choices, won't build a barracks unless we have a minimum of workers. Won't gather resources too far away.

--- bot/src/bot/WorkersForLife.java
+++ bot/src/bot/WorkersForLife.java
@@ -124,13 +124,13 @@ public class WorkersForLife extends AbstractionLayerAI
         /// Cycle through all ranged units and apply behaviour
         for (Unit u : ranged) 
         {
-        	battleUnitBehavior(u, p, gs);
+        	battleUnitBehavior(u, p, pgs);
         	/// If this unit is getting stuck next to the barracks, it will be stacked up
         	if (barracks != null)
         	{
 	        	if (getDistance(barracks, u) == 1)
 	        	{
-	        		stackUnits(u, gs, 1, base);
+	        		stackUnits(u, gs, 1, base, 5);
 	        	}
         	}
         }
@@ -138,7 +138,7 @@ public class WorkersForLife extends AbstractionLayerAI
         /// Cycle through all light units and apply behaviour
         for (Unit u : light) 
         {
-        	battleUnitBehavior(u, p, gs);
+        	battleUnitBehavior(u, p, pgs);
         }
         
     	/// If our base is not building something then apply base behaviour
@@ -162,7 +162,7 @@ public class WorkersForLife extends AbstractionLayerAI
      * Called when the barracks is not building anything, will try to build light or ranged units.
      * Expand this behaviour if wanting to bring in different unit types or build orders.
      * @param barracks Our barracks
-     * @param p Our player id
+     * @param p Our player
      */
     public void barracksBehaviour(Unit barracks, Player p) {
     	/// If enough resources train ranged unit
@@ -184,7 +184,7 @@ public class WorkersForLife extends AbstractionLayerAI
     /** Behaviour for base.
      * Builds new workers if we have the resources and are not over the maxWorkers
      * @param base Our base
-     * @param p Our player id
+     * @param p Our player
      * @param ourWorkers Number of workers we have
      * @param maxWorkers Max number of workers we want to have
      */
@@ -200,7 +200,7 @@ public class WorkersForLife extends AbstractionLayerAI
      * Takes the list of workers and delegates them to either free or battle workers depending on the current state of the game
      * Then applies behaviours to those individual lists. Here we deal with harvesting, and building barracks or bases.
      * @param workers List of all our workers
-     * @param p Our player id
+     * @param p Our player
      * @param pgs The PhysicalGameState
      * @param gs The GameState
      * @param base Our base
@@ -210,6 +210,7 @@ public class WorkersForLife extends AbstractionLayerAI
         					with the idea of expansion in mind. */
         int nbarracks = 0; /**< Number of barracks we have, currently will only ever be 0 or 1 but has been designed
 								with the idea of expansion in mind. */
+        int nworkers = workers.size(); ///< Number of workers we have
         int nresources = 0;	///< Number of resource piles on the map
         int workerOffset = 0; ///< Allocates more free workers set to higher for bigger maps
         
@@ -283,7 +284,7 @@ public class WorkersForLife extends AbstractionLayerAI
         if (nbarracks == 0 && !freeWorkers.isEmpty())
         {
         	/// Build a barracks if we have enough resources
-            if (p.getResources() >= 6) {
+            if (p.getResources() >= 6 && nworkers >= 5) {
             	Unit u = freeWorkers.remove(0);
             	/// Do a null check on the base 
             	if (base != null)
@@ -317,7 +318,7 @@ public class WorkersForLife extends AbstractionLayerAI
         /// Send battle workers to battle
         for (Unit u : battleWorkers) 
         {
-        	battleUnitBehavior(u, p, gs);
+        	battleUnitBehavior(u, p, pgs);
         }
         
         /// If we have a certain number of battleWorkers start stacking some out of the way
@@ -328,7 +329,7 @@ public class WorkersForLife extends AbstractionLayerAI
         	{
         		if (counter >= 2)
         		{
-        			stackUnits(u, gs, 0, base);
+        			stackUnits(u, gs, 0, base, 5);
         		}
         		counter++;
         	}
@@ -345,11 +346,10 @@ public class WorkersForLife extends AbstractionLayerAI
      * Used to send a unit to fight, gets the closest base and enemy unit.
      * Prioritises killing units over bases, always aiming for the closest one.
      * @param u Our unit we are commanding
-     * @param p Our player id
-     * @param gs The GameState
+     * @param p Our player
+     * @param pgs The GameState
      */
-    public void battleUnitBehavior(Unit u, Player p, GameState gs) {
-        PhysicalGameState pgs = gs.getPhysicalGameState();
+    public void battleUnitBehavior(Unit u, Player p, PhysicalGameState pgs) {
         /// Get the closest enemy unit and base
         Map<String, Unit> closestBaseAndEnemy = getClosestEnemy(pgs, u, p);
         /// Allocate unit and base to variables to avoid additional look ups
@@ -368,31 +368,20 @@ public class WorkersForLife extends AbstractionLayerAI
         }
     }
     
-    /*===================Utility=============*/
-    
+    /** Tells workers in a list to harvest resources.
+     * Gets the closest resource pile and closest base and orders workers to harvest between them.
+     * @param freeWorkers List of workers we want to harvest
+     * @param pgs The PhysicalGameSatet
+     * @param p Our player
+     */
     private void workerHarvest(List<Unit> freeWorkers, PhysicalGameState pgs, Player p)
     {
     	for (Unit u : freeWorkers) 
         {
-            Unit closestBase = null;
-            Unit closestResource = null;
-            int closestDistance = 0;
-            // Get closest resource
-            for (Unit u2 : pgs.getUnits()) 
-            {
-                if (u2.getType().isResource) 
-                {
-                    int d = getDistance(u,u2);
-                    if (closestResource == null || d < closestDistance) 
-                    {
-                        closestResource = u2;
-                        closestDistance = d;
-                    }
-                }
-            }
-            // Reset closestDistance
-            closestDistance = 0;
-            // Get closestBase
+            Unit closestBase = null; ///< The closest base to the worker
+            Unit closestResource = null; ///< The closest resource pile to the worker
+            int closestDistance = 0; ///< Current closest distance
+            /// Get closest base
             for (Unit u2 : pgs.getUnits()) 
             {
                 if (u2.getType().isStockpile && u2.getPlayer() == p.getID()) 
@@ -405,28 +394,59 @@ public class WorkersForLife extends AbstractionLayerAI
                     }
                 }
             }
-            // If we have a base and resource then harvest!
-            if (closestResource != null && closestBase != null) 
+            /// Reset closestDistance
+            closestDistance = 0;
+            /// Get closest resource
+            for (Unit u2 : pgs.getUnits()) 
+            {
+                if (u2.getType().isResource) 
+                {
+                    int d = getDistance(u,u2);
+                    if (closestResource == null || d < closestDistance) 
+                    {
+                        closestResource = u2;
+                        closestDistance = d;
+                    }
+                }
+            }
+            /// If we have a base and resource then harvest, so long as the resource isn't too far away
+            if (closestResource != null && closestBase != null && closestDistance <= 6) 
             {
             	harvest(u, closestResource, closestBase);
+            }
+            else
+            {
+            	battleUnitBehavior(u,p,pgs);
             }
         }
     }
     
-    // Gets the closest enemy and base to the given unit
+    /*===================Utility===============*/
+    
+    /** Gets the closest enemy unit and enemy base.
+     * Finds the closest enemy to the given unit, looks for units and bases and returns both the closest
+     * unit and base in a Map format
+     * @param pgs The PhysicalGameState
+     * @param u The unit to check from
+     * @param p Our player
+     * @return Returns a Map with [key:unit] keys are "enemy" or "base"
+     */
     private Map<String, Unit> getClosestEnemy(PhysicalGameState pgs, Unit u, Player p)
     {
-    	Map<String, Unit> closestBaseAndEnemy = new HashMap<String, Unit>();
-        int closestDistance = 10;
+    	Map<String, Unit> closestBaseAndEnemy = new HashMap<String, Unit>(); ///< Map to hold the enemy and base
+        int closestDistance = 10; ///< The current closest distance
+        /// Populate the closestBaseAndEnemy Map
         for (Unit u2 : pgs.getUnits()) 
         {
             if (u2.getPlayer() >= 0 && u2.getPlayer() != p.getID()) 
             {
                 int d = getDistance(u,u2);
+                /// If the unit is a base
                 if (u2.getType() == baseType)
                 {
                 	closestBaseAndEnemy.put("base",u2);
                 }
+                /// If the unit is another type of enemy
                 else if (closestBaseAndEnemy.get("enemy") == null || d < closestDistance) 
                 {
                 	closestBaseAndEnemy.put("enemy",u2);
@@ -437,32 +457,58 @@ public class WorkersForLife extends AbstractionLayerAI
         return closestBaseAndEnemy;
     }
     
-    // Checks if we are on the left of the map
+    /** Check what side of the map we are on.
+     * Returns true if we are on the left side of the map, and false if we are on the right
+     * Subtracts half the map width from our base's X position, if positive we are on the right
+     * @param pgs The PhysicalGameState
+     * @param base Our base
+     * @return True if on the left, false if on the right
+     */
     private boolean areWeOnTheLeft(PhysicalGameState pgs, Unit base) {
+    	/// Get the bases's X position
         int baseX = base.getX();
+        /// Get half the map width
         int xLocation = baseX - (pgs.getWidth()/2);
-        // If positive we are on the right
+        /// If positive we are on the right
         if (xLocation >= 0)
         {
             return false;
         }
-        // If negative we are on the left
+        /// If negative we are on the left
         else
         {
             return true;
         }
     }
     
+    /** Gets the distance between two units.
+     * Checks the differences between X and Y positions of two units and adds them together to return a single int
+     * @param u The first unit to check
+     * @param u2 The second unit to check
+     * @return The distance value as a single int
+     */
     private int getDistance(Unit u, Unit u2) {
     	int distance = Math.abs(u2.getX() - u.getX()) + Math.abs(u2.getY() - u.getY());
     	return distance;
     }
     
-    // Stacks units up out of the way for when they can't attack
-    private void stackUnits(Unit u, GameState gs, int offset, Unit base) {
+    /** Stacks units up out of the way for when they can't attack.
+     * Used when we have units not doing anything to get them out the way of the base or barracks.
+     * Checks if we are on the left or right then stacks the units below of above the base respectively.
+     * Checks that there is a path that the unit can use to get to that location, this avoids units trying to go
+     * to already used locations for stacking.
+     * @param u The unit we want to stack
+     * @param gs The GameState
+     * @param offset This offset moves the Y value of stacking position so units can be stacked in different columns
+     * @param base Our base
+     * @param loops The numbers of tries to stack a unit, each tries moves the x value by one to find a free space
+     */
+    private void stackUnits(Unit u, GameState gs, int offset, Unit base, int loops) {
         PhysicalGameState pgs = gs.getPhysicalGameState();
+        /// Check what side of the map we are on
     	boolean onTheLeft = areWeOnTheLeft(pgs, base);
-    	for (int n = 0; n < 5; n++)  {
+    	/// Try to stack unit on the correct side if a path exists
+    	for (int n = 0; n < loops; n++)  {
         	if (onTheLeft) {
         		boolean checkPath = pf.pathExists(u, ((0 + offset)+(pgs.getHeight()-n)*pgs.getWidth()), gs, null);
         		if (checkPath) {
